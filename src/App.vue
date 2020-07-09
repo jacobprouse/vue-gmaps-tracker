a1df590bcd346937b512b9dc674cebf78a2ab641
Jacob Prouse <jacob.a.prouse@gmail.com>
2020-07-08 20:42:07 -0400

    Set up project
    Initialized project with vue-cli. Added an initial maps loader, some beginner locations, and a list of clickable items that set the center of the map.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,95 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>
+      You are currently at: <span class="location">{{ currentLocation.name }}</span>
+    </p>
+    <div class="inline-map">
+      <ul class="location-list">
+        <li v-for="(location, index) in locations" :key="index" @click="goTo(location)">
+          {{ location.name }}
+        </li>
+      </ul>
+      <div id="map" ref="map" class="main-map" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import gmapsInit from '@/utils/gmaps'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      locations: [
+        {lat: 43.653225, lng: -79.383186, name:'Toronto'},
+        {lat: 43.653225, lng: -89.383186, name:'Toronto2'},
+        {lat: 43.653225, lng: -99.383186, name:'Toronto3'}
+      ],
+      currentLocation: null,
+      map: null
+    }
+  },
+  created () {
+    this.currentLocation = this.locations[0]
+  },
+  async mounted() {
+    try {
+      const google = await gmapsInit()
+      this.map = new google.maps.Map(this.$refs.map,
+      {
+        center: this.currentLocation,
+        zoom: 8
+      })
+
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  methods: {
+    goTo (location) {
+      this.currentLocation = location
+      this.map.setCenter(this.currentLocation)
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .inline-map {
+    display: inherit;
+    flex-direction: row;
+
+    .location-list {
+      padding-right: 20px;
+      list-style: none;
+
+      li {
+        border: 2px solid red;
+        margin: 5px 0;
+        padding: 5px 0;
+      }
+    }
+
+    #map {
+      width: 500px;
+      height: 500px;
+      border: 1px solid black;
+    }
+
+    .location {
+      color: green;
+      border: 1px solid green;
+    }
+  }
 }
 </style>
---
+++ src/components/Map.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="map" class="main-map" />
+</template>
+
+<script>
+import gmapsInit from '@/utils/gmaps';
+
+export default {
+  name: 'Map',
+  data () {
+    return {
+      locations: [
+        {lat: 43.745529, lng: -81.712486},
+        {lat: 43.745529, lng: -91.712486},
+        {lat: 43.745529, lng: -101.712486}
+      ]
+    }
+  },
+  async mounted() {
+    try {
+      // const google = await gmapsInit();
+      // const map = new google.maps.Map(this.$el,
+      // {
+      //   center: this.locations[0],
+      //   zoom: 8
+      // })
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .main-map {
+    width: 600px;
+    height: 600px;
+  }
+</style>
