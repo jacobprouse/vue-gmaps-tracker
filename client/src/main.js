5fdcd940fc78039d2d1ad9b489835029bd360904
Jacob Prouse <jacob.a.prouse@gmail.com>
2020-09-19 00:30:54 -0400

    added url for contacting server

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
+import axios from 'axios'
 import App from '@/components/App.vue'
 import dotenv from 'dotenv'
 
+axios.defaults.baseURL = process.env.VUE_APP_SERVER_ORIGIN
 dotenv.config()
 
 Vue.config.productionTip = false
