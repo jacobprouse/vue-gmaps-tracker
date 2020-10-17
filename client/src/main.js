3fb143db223190ae01c9eefccaa7e762c0c452e4
Jacob Prouse <jacob.a.prouse@gmail.com>
2020-10-17 16:00:13 -0400

    Added font awesome

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,20 @@ import Vue from 'vue'
 import axios from 'axios'
 import App from '@/components/App.vue'
 import dotenv from 'dotenv'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faCog,
+  faGlobe,
+  faMapMarkedAlt
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add([
+  faCog,
+  faGlobe,
+  faMapMarkedAlt
+])
+Vue.component('fa-icon', FontAwesomeIcon)
 
 axios.defaults.baseURL = process.env.VUE_APP_SERVER_ORIGIN
 dotenv.config()
