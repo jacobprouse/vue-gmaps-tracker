43f2c3eae5be3123d820e767696b9261c2cd3003
Jacob Prouse <jacob.a.prouse@gmail.com>
2020-10-17 20:06:50 -0400

    Correctly set viewport height of app

--- client/src/components/App.vue
+++ client/src/components/App.vue
@@ -1,19 +1,48 @@
 <template>
-  <div
-    id="app"
-    class="container"
-  >
-    <Container />
+  <div class="app">
+    <Header :loading="loading" />
+    <Container :loading="loading" />
   </div>
 </template>
 
 <script>
-import Container from '@/components/Container.vue'
+import Container from '@/components/Container'
+import Header from '@/components/Header'
 
 export default {
   name: 'App',
   components: {
-    Container
+    Container,
+    Header
+  },
+  data () {
+    return {
+      loading: true
+    }
   }
 }
 </script>
+<style lang="scss">
+@import '@/scss/mixins/breakpoints';
+@import '@/scss/variables';
+
+// Get Full Viewport height.
+html { height: 100%; }
+
+body {
+  background-color: $secondary;
+  bottom: 0;
+  left: 0;
+  position: absolute;
+  right: 0;
+  top: 0;
+}
+
+// Main Component
+.app {
+  background-color: $primary;
+  border-radius: 20px;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
